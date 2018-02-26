4e1bfea99df73a33f75b955e61d0ce792d64ab52
Ivan Savushkin <ivan@savushkin.me>
2018-02-26 21:57:04 +0300

    java ee webservice

--- lab1/source/lab1/lab1-cli-client/src/main/java/me/savushkin/wst/App.java
+++ lab1/source/lab1/lab1-cli-client/src/main/java/me/savushkin/wst/App.java
@@ -11,50 +11,48 @@ import java.util.logging.Logger;
  */
 public class App {
     private static void print(List<MusicAlbum> musicAlbums) {
-        musicAlbums.forEach(musicAlbum -> System.out.printf(
-                "ID: %d,%n" +
-                        "Альбом: %s,%n" +
-                        "Исполнитель: %s,%n" +
-                        "Год выпуска: %d,%n" +
-                        "Жанр: %s,%n" +
-                        "Количество записей: %d.%n%n",
+        String separator = "---------------------------------------------------------------------------------------------------------------------------";
+        System.out.println(separator);
+        System.out.printf("| %5s | %30s | %30s | %11s | %10s | %18s |%n",
+                "ID",
+                "Альбом",
+                "Исполнитель",
+                "Год выпуска",
+                "Жанр",
+                "Количество записей");
+        System.out.println(separator);
+        musicAlbums.forEach(musicAlbum -> System.out.printf("| %5d | %30s | %30s | %11d | %10s | %18d |%n",
                 musicAlbum.getId(),
                 musicAlbum.getTitle(),
                 musicAlbum.getArtist(),
                 musicAlbum.getReleaseYear(),
                 musicAlbum.getGenre(),
                 musicAlbum.getTrackCount()));
-        System.out.printf("Всего: %d.%n---------------------------%n%n", musicAlbums.size());
+        System.out.println(separator);
+        System.out.printf("Всего: %d.%n%n", musicAlbums.size());
     }
 
     public static void main(String[] args) {
         try {
             URL url = new URL("http://0.0.0.0:8080/music-album-service?wsdl");
             MusicAlbumService musicAlbumService = new MusicAlbumService(url);
-            System.out.printf("Все альбомы:%n%n");
+            System.out.printf("Все альбомы:%n");
             print(musicAlbumService.getMusicAlbumWebServicePort().getAlbums());
-            System.out.printf("Поиск без параметров:%n%n");
-            print(musicAlbumService.getMusicAlbumWebServicePort().findAlbums(
-                    null,
-                    null,
-                    null,
-                    null,
-                    null));
-            System.out.printf("Поиск по жанру \"Blues\":%n%n");
+            System.out.printf("Поиск по жанру \"Blues\":%n");
             print(musicAlbumService.getMusicAlbumWebServicePort().findAlbums(
                     null,
                     null,
                     null,
                     "Blues",
                     null));
-            System.out.printf("Поиск по году \"1969\":%n%n");
+            System.out.printf("Поиск по году \"1969\":%n");
             print(musicAlbumService.getMusicAlbumWebServicePort().findAlbums(
                     null,
                     null,
                     1969L,
                     null,
                     null));
-            System.out.printf("Поиск по году \"1969\" и жанру \"Blues\":%n%n");
+            System.out.printf("Поиск по году \"1969\" или жанру \"Blues\":%n");
             print(musicAlbumService.getMusicAlbumWebServicePort().findAlbums(
                     null,
                     null,
--- lab1/source/lab1/lab1-j2ee/src/test/java/me/savushkin/wst/InitializerBeanTest.java
+++ lab1/source/lab1/lab1-j2ee/src/test/java/me/savushkin/wst/InitializerBeanTest.java
@@ -5,16 +5,16 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple InitializerBean.
  */
-public class AppTest
+public class InitializerBeanTest
         extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest(String testName) {
+    public InitializerBeanTest(String testName) {
         super(testName);
     }
 
@@ -22,7 +22,7 @@ public class AppTest
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite(AppTest.class);
+        return new TestSuite(InitializerBeanTest.class);
     }
 
     /**
